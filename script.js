53ae1d70b36e7934b2d6f1fc9e4769936ec2a477
Sgamb29 <shagamb@gmail.com>
2024-11-18 12:54:02 -0700

    better fetch

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ async function counterCall() {
         method: "POST",
         body: "sgambappsmain",
     });
+    try {
+        const response = await fetch(request);
 
-    const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`Response Status: ${response.status}`);
+
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 counterCall();
