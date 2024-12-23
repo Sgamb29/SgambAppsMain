a536895d45829e44c8abbcf91557d8703197a821
Sgamb29 <shagamb@gmail.com>
2024-12-23 16:18:53 -0700

    updates and traffic count

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// Traffic
+
+const request = new Request("https://server.sgambapps.com", {
+    method: "POST",
+    headers: {
+        "Content-Type": "text/plain"
+    },
+    body: "sgambapps",
+});
+fetch(request)
+.then(res => {
+    if (res.ok) {
+    console.log("visit counted");
+    }
+})
+.catch(err => console.log(err));
