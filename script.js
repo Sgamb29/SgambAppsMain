7d30d96b0a7a9b43845d4f85abdedfa59c399b9d
Sgamb29 <shagamb@gmail.com>
2025-01-03 14:43:40 -0700

    added typing site

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 
 // Traffic
-
 const request = new Request("https://server.sgambapps.com/?site=sgambapps", {
     method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({site: "sgambapps"}),
 });
 fetch(request)
 .then(res => {
